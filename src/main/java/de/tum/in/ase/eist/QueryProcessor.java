20f505cdfaebab7d4212c365d4e83b967024ad07
ge47mut <ge47mut@mytum.de>
2022-07-15 11:32:32 +0200

    please

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -15,8 +15,16 @@ public class QueryProcessor {
            return "Taha";
         } else if (query.contains("plus")) { // TODO extend the programm here
             String[] queryStuff = query.split(" ");
-            Integer result = Integer.parseInt(queryStuff[2]) + Integer.parseInt(queryStuff[4]);
-            return result.toString();
+            Integer result = 0;
+            try {
+                result = Integer.parseInt(queryStuff[2]) + Integer.parseInt(queryStuff[4]);
+            }
+            catch (Exception e) {
+                result = 0;
+            }
+            finally {
+                return result.toString();
+            }
         }
         else if (query.contains("largest")) {
             query = query.substring(47);
